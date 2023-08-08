1f7c065f4356972298bc1a633bffa56e8abde72d
dahye <bbngsk@gmail.com>
2023-08-08 19:19:52 +0900

    랜덤 맥주 추천 기능 구현 완료

--- BeerProject/ViewController.swift
+++ BeerProject/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet var beerImageView: UIImageView!
+    @IBOutlet var beerNameLabel: UILabel!
+    @IBOutlet var beerDescription: UILabel!
+    @IBOutlet var randomBeerButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        randomBeerButtonDidTap(randomBeerButton)
     }
 
+    @IBAction func randomBeerButtonDidTap(_ sender: UIButton) {
+
+        let url = "https://api.punkapi.com/v2/beers/random"
+
+        AF.request(url, method: .get).validate().responseJSON { response in
+                    switch response.result {
+                    case .success(let value):
+                        let json = JSON(value)
+                        print("JSON: \(json)")
+
+                        let name = json[0]["name"].stringValue
+                        let description = json[0]["description"].stringValue
+                        let imageUrlString = json[0]["image_url"].stringValue
+                        let imageUrl = URL(string: imageUrlString)
+
+                        self.beerNameLabel.text = name
+                        self.beerDescription.text = description
+                        self.beerImageView.kf.setImage(with: imageUrl)
+
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+
+    }
 
 }
 
